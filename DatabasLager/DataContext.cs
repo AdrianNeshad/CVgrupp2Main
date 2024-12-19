e6d2abe5b505faf6cac3c9b7980760c5bb2c23fb
UCPolisen <168416050+AdrianNeshad@users.noreply.github.com>
2024-12-19 15:20:20 +0100

    a

--- DatabasLager/DataContext.cs
+++ DatabasLager/DataContext.cs
@@ -1,6 +1,13 @@
-﻿namespace CVgrupp2Main.DatabasLager
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CVgrupp2Main.DatabasLager
 {
-    public class DataContext
+    public class DataContext : DbContext<ApplicationUser>
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+
+
     }
 }
