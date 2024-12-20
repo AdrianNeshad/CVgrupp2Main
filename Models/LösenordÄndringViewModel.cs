107cd818134d86de8577bb9d30346ca72c6d71a2
ozansoyal <169032154+ozansoyal@users.noreply.github.com>
2024-12-20 14:28:27 +0100

    lösenordsändring

--- Models/LösenordÄndringViewModel.cs
+++ Models/LösenordÄndringViewModel.cs
@@ -1,6 +1,21 @@
-﻿namespace CVgrupp2Main.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CVgrupp2Main.Models
 {
     public class LösenordÄndringViewModel
     {
+        [Required(ErrorMessage = "Skriv in ditt nya lösenord.")]
+        [DataType(DataType.Password)]
+        public string gammaltLösenord { get; set; }
+
+        [Required(ErrorMessage = "Skriv in ditt nya lösenord.")]
+        [DataType(DataType.Password)]
+        [Compare("bekräftaNyttLösenord")]
+        public string nyttLösenord { get; set; }
+
+        [Required(ErrorMessage = "Bekräfta ditt nya lösenord")]
+        [DataType(DataType.Password)]
+        [Compare("bekräftaNyttLösenord")]
+        public string bekräftaNyttLösenord { get; set; }
     }
 }
