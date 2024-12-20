cd7c215318cc99194ebe5826f72615c9274d4f54
UCPolisen <168416050+AdrianNeshad@users.noreply.github.com>
2024-12-20 11:58:29 +0100

    a

--- DatabasLager/Kontaktuppgifter.cs
+++ DatabasLager/Kontaktuppgifter.cs
@@ -1,11 +1,13 @@
-﻿namespace CVgrupp2Main.DatabasLager
+﻿using System.ComponentModel.DataAnnotations;
+namespace CVgrupp2Main.DatabasLager
 {
     public class Kontaktuppgifter
     {
+        [Key]
         public int KontaktID { get; set; }
-        public string Telefonnummer { get; set; }
-        public string Adress { get; set; }
-        public string Email { get; set; }
+        public string? Telefonnummer { get; set; }
+        public string? Adress { get; set; }
+        public string? Email { get; set; }
         public virtual Person Person { get; set; }
     }
 }
--- DatabasLager/Utbildningar.cs
+++ DatabasLager/Utbildningar.cs
@@ -1,10 +1,12 @@
-﻿namespace CVgrupp2Main.DatabasLager
+﻿using System.ComponentModel.DataAnnotations;
+namespace CVgrupp2Main.DatabasLager
 {
     public class Utbildningar
     {
+        [Key]
         public int UtbildningID { get; set; }
-        public string Namn { get; set; }
-        public string Beskrivning { get; set; }
+        public string? Namn { get; set; }
+        public string? Beskrivning { get; set; }
         public virtual IEnumerable<PersonUtbildningar> HarUtbildning { get; set; } = new List<PersonUtbildningar>();
     }
 }
