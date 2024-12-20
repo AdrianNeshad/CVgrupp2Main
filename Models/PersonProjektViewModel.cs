c3bcaa6d6a4766daf0f4f8a76ead05855ef5b1d9
ozansoyal <169032154+ozansoyal@users.noreply.github.com>
2024-12-20 14:17:06 +0100

    NU

--- Models/PersonProjektViewModel.cs
+++ Models/PersonProjektViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace CVgrupp2Main.Models
+﻿using CVgrupp2Main.DatabasLager;
+
+namespace CVgrupp2Main.Models
 {
     public class PersonProjektViewModel
     {
+        public List<Person> person { get; set; }
+        public List<Projekt> projekts { get; set; }
+        public List<PersonProjekt> projekt { get; set; }
+        //hej
     }
 }
