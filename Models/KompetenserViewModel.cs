2be9ea7dfd929656d5b12546fc1868a0e9ce3692
UCPolisen <168416050+AdrianNeshad@users.noreply.github.com>
2024-12-20 13:54:20 +0100

    kompetensviewmodel

---
+++ Models/KompetenserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CVgrupp2Main.Models
+{
+    public class KompetenserViewModel
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Fyll i kompetenstitel")]
+        [StringLength(50, ErrorMessage = "Kompetenstitel får vara max 50 karaktärer")]
+        public string Titel { get; set; }
+
+        [Required(ErrorMessage = "Fyll i kompetensbeskrivning")]
+        [StringLength(300, ErrorMessage = "Kompetensbeskrivning får vara max 300 karaktärer")]
+        public string Beskrivning { get; set; }
+    }
+}
