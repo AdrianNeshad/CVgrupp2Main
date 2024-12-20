1cf26eb6ccb401eea59ab03c06856e445d290e77
UCPolisen <168416050+AdrianNeshad@users.noreply.github.com>
2024-12-20 13:45:48 +0100

    a

---
+++ Models/ErfarenheterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CVgrupp2Main.Models
+{
+    public class ErfarenheterViewModel
+    {
+
+        [Required(ErrorMessage = "PersonID måste anges.")]
+        public string ID { get; set; }
+
+        [Required(ErrorMessage = "Erfarenhetens titel måste anges.")]
+        public string Titel { get; set; }
+
+        [Required(ErrorMessage = "Erfarenhetsbeskrivning måste anges.")]
+        public string Beskrivning { get; set; }
+    }
+}
