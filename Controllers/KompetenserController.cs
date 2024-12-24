76e1df8cae370286058f1563a8089f6537f8c233
UCPolisen <168416050+AdrianNeshad@users.noreply.github.com>
2024-12-24 13:19:04 +0100

    a

--- Controllers/KompetenserController.cs
+++ Controllers/KompetenserController.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using CVgrupp2Main.Models;
+using CVgrupp2Main.DatabasLager;
 
 namespace CVgrupp2Main.Controllers
 {
+    [Authorize]
     public class KompetenserController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        
     }
 }
