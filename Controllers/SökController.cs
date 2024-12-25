54936c90fa10e25f12773d2b38a1e63ea8a3d389
UCPolisen <168416050+AdrianNeshad@users.noreply.github.com>
2024-12-25 13:58:36 +0100

    sökcontroller

--- Controllers/SökController.cs
+++ Controllers/SökController.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using CVgrupp2Main.DatabasLager;
+using Microsoft.EntityFrameworkCore;
 
 namespace CVgrupp2Main.Controllers
 {
-    public class SökController : Controller
+    public class SökController(DataContext data) : Controller
     {
-        public IActionResult Index()
+        public IActionResult SökVy()
         {
+            ViewBag.AntalMeddelanden = data.PersonMottagitMeddelande.Where(m => m.Användarnamn == User.Identity.Name && !m.Meddelande.HarLästs).Count();
             return View();
+        }
+
+        // Action för att hantera sökningen
+        public IActionResult Sök(string sökning)
+        {
+            var users = data.Person
+                        .Where(p => p.Förnamn.Contains(sökning) || p.Efternamn.Contains(sökning))
+                        .ToList();
+
+
+            // Om användaren inte är inloggad, hämta endast offentliga profiler
+            if (User.Identity.Name == null)
+            {
+                users = users.Where(p => !p.Privat).ToList();
+            }
+
+            ViewBag.AntalMeddelanden = data.PersonMottagitMeddelande.Where(m => m.Användarnamn == User.Identity.Name && !m.Meddelande.HarLästs).Count();
+            // Skicka resultatet till vyn
+            return View("Sökresultat", users);
         }
     }
 }
