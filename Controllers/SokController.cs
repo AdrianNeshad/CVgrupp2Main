5d64ab6ab26a4db5331dce2655e4705fe4cacd95
UCPolisen <168416050+AdrianNeshad@users.noreply.github.com>
2025-01-06 13:03:04 +0100

    a

--- Controllers/SokController.cs
+++ Controllers/SokController.cs
@@ -11,13 +11,12 @@ namespace CVgrupp2Main.Controllers
             return View();
         }
 
-        // Action för att hantera sökningen
         public IActionResult Sök(string sökning)
         {
-            var users = data.Person
-                        .Where(p => p.Förnamn.Contains(sökning) || p.Efternamn.Contains(sökning))
-                        .ToList();
-
+            // Hämta alla användare om sökning är null eller tom
+            var users = string.IsNullOrWhiteSpace(sökning)
+                ? data.Person.ToList()
+                : data.Person.Where(p => p.Förnamn.Contains(sökning) || p.Efternamn.Contains(sökning)).ToList();
 
             // Om användaren inte är inloggad, hämta endast offentliga profiler
             if (User.Identity.Name == null)
@@ -25,9 +24,12 @@ namespace CVgrupp2Main.Controllers
                 users = users.Where(p => !p.Privat).ToList();
             }
 
+            // Räkna olästa meddelanden för inloggad användare
             ViewBag.AntalMeddelanden = data.PersonMottagitMeddelande.Where(m => m.Användarnamn == User.Identity.Name && !m.Meddelande.HarLästs).Count();
+
             // Skicka resultatet till vyn
             return View("Sökresultat", users);
         }
+
     }
 }
